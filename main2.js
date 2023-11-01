3e6997a5faaf8976969c746436e184c2e61742e4
Desmond-Zeh <148614390+Desmond-Zeh@users.noreply.github.com>
2023-11-01 10:30:46 +0100

    Update main2.js

--- main2.js
+++ main2.js
@@ -1,12 +1,9 @@
 /* My main js code*/
-
-let myheading=document.querySelector('h1');
-let newheading="Welcome to the energy metering website!";
-let finalheading=newheading.toUpperCase();
-myheading.textContent=finalheading;
 /* Wait for the page to be fully loaded before the image switching operation commences*/
-window.onload = CommenceEvent;
-let myImagesArray = ["TC.png","TT.png","SL7000.jpg","RS232.jpg","CB.png","surge.png"];
+window.onload =() =>{
+ CommenceEvent();
+}
+ let myImagesArray = ["TC.png","TT.png","SL7000.jpg","RS232.jpg","CB.png","surge.png"];
 let myImageIndex = 0;
 let timeInterval = 2000;
 function CommenceEvent(){
