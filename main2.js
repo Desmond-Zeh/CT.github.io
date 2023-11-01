1769ccd129fce37bca7e7251e1b9acd3ba657a74
Desmond-Zeh <148614390+Desmond-Zeh@users.noreply.github.com>
2023-11-01 11:39:19 +0100

    Add files via upload

---
+++ main2.js
@@ -0,0 +1,32 @@
+/* My main js code*/
+
+/* Wait for the page to be fully loaded before the image switching operation commences*/
+window.onload = () =>{
+ CommenceEvent();
+};
+let myImagesArray = ["TC.png","TT.png","SL7000.jpg","RS232.jpg","CB.png","surge.png"];
+let myImageIndex = 0;
+let timeInterval = 2000;
+function CommenceEvent(){
+document.myImages.src=myImagesArray[myImageIndex];
+
+if(myImageIndex < myImagesArray.length - 1){
+myImageIndex ++;
+}
+else{
+ResetImages();
+}
+ setTimeout("CommenceEvent()", timeInterval);
+ }
+function ResetImages(){
+myImageIndex = 0;
+  }
+
+ let mydoc = document.getElementById("myDoc");
+mydoc.onclick = () =>{
+window.location.replace("https://desmond-zeh.github.io/Des.github.io/");
+};
+ let myhom = document.getElementById("home");
+myhom.onclick = () =>{
+window.location.replace("");
+};
